09607f9bb3f582a0f127a594347c75dd082e1554
ssvikash_20 <arisivikash@gmail.com>
2024-12-29 16:03:15 +0530

    Create SQL query for users

--- internal/store/users.go
+++ internal/store/users.go
@@ -17,6 +17,21 @@ type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(context.Context) error {
-	return nil
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO USERS (username, password, email) VALUES ($1, $2, $3) RETURNING id, created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	return err
 }
